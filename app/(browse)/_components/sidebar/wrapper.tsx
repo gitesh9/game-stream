47588fb2b4f76192179495fcea5ac5994079187e
Gitesh <giteshwan98@gmail.com>
2024-08-23 13:02:27 +0530

    Created Side bar and resolved some bugs

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,13 +1,36 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { cn } from "@/lib/utils";
 import { useSideBar } from "@/store/use-sidebar";
+
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 interface WrapperProps {
 	children: React.ReactNode;
 }
 export const Wrapper = ({ children }: WrapperProps) => {
+	const [isClient, setIsClient] = useState(false);
 	const { collapsed } = useSideBar((state) => state);
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
+	if (!isClient) {
+		return (
+			<aside
+				className={cn(
+					"fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[2D2E35] z-50"
+				)}
+			>
+				<ToggleSkeleton />
+				<RecommendedSkeleton />
+			</aside>
+		);
+	}
 	return (
 		<aside
 			className={cn(

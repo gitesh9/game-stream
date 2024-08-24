41ad0b343891b74edcc9de9685b9eaf8b99ffd2a
Gitesh <giteshwan98@gmail.com>
2024-08-24 10:33:55 +0530

    added block funtionality

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { onBlock, onunBlock } from "@/actions/block";
 import { onFollow, onUnFollow } from "@/actions/follow";
 import { Button } from "@/components/ui/button";
 import { useTransition } from "react";
@@ -44,9 +45,25 @@ export const Actions = ({ isFollowing, userId }: ActionsProps) => {
 		}
 	};
 
+	const handleBlock = () => {
+		startTransition(() => {
+			onunBlock(userId)
+				.then((data) =>
+					toast.success(`Blocked the user ${data.blocked.username}`)
+				)
+				.catch(() => toast.error("Something went wrong"));
+		});
+	};
+
 	return (
-		<Button disabled={isPending} onClick={onClick} variant="primary">
-			{isFollowing ? "Unfollow" : "Follow"}
-		</Button>
+		<>
+			<Button disabled={isPending} onClick={onClick} variant="primary">
+				{isFollowing ? "Unfollow" : "Follow"}
+			</Button>
+			<Button disabled={isPending} onClick={handleBlock}>
+				{/* {isFollowing ? "Unfollow" : "Follow"} */}
+				block User
+			</Button>
+		</>
 	);
 };

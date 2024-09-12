44a4d23c1ffdfa80ded18a0af1b4fdbf0d42b7a1
Gitesh <giteshwan98@gmail.com>
2024-09-12 17:36:06 +0530

    community component and stream page skeleton created

--- components/stream-player/chat.tsx
+++ components/stream-player/chat.tsx
@@ -11,9 +11,10 @@ import { ConnectionState } from "livekit-client";
 
 import { ChatVariant, useChatSideBar } from "@/store/use-chat-sidebar";
 
-import { ChatHeader } from "./chat-header";
-import { ChatForm } from "./chat-form";
-import { ChatList } from "./chat-list";
+import { ChatHeader, ChatHeaderSkeleton } from "./chat-header";
+import { ChatForm, ChatFormSkeleton } from "./chat-form";
+import { ChatList, ChatListSkeleton } from "./chat-list";
+import { ChatCommunity } from "./chat-community";
 
 interface ChatProps {
 	viewerName: string;
@@ -87,9 +88,23 @@ export const Chat = ({
 			)}
 			{variant === ChatVariant.COMMUNITY && (
 				<>
-					<p>Community</p>
+					<ChatCommunity
+						viewerName={viewerName}
+						hostName={hostName}
+						isHidden={isHidden}
+					/>
 				</>
 			)}
+		</div>
+	);
+};
+
+export const ChatSkeleton = () => {
+	return (
+		<div className="flex flex-col border-l border-b pt-0 h-[calc(100vh-80px)] border-2">
+			<ChatHeaderSkeleton />
+			<ChatListSkeleton />
+			<ChatFormSkeleton />
 		</div>
 	);
 };
